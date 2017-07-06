ab19ca03c311fcf983e3bd553c5636ef0583e30f
Ryota Murofushi <pizzaman8823g@gmail.com>
2017-07-06 02:15:07 +0000

    Add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'messages#index'
   resources :messages
   resources :massages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
