f3cd0344abc6e5486e8618e71f8cd801e9027bc4
Ryota Murofushi <pizzaman8823g@gmail.com>
2017-07-06 03:05:01 +0000

    Add name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706015647) do
+ActiveRecord::Schema.define(version: 20170706025904) do
 
   create_table "massages", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170706015647) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
